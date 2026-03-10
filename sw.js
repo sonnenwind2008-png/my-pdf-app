49118c0104dbfb4183ec91b4ac585df2747460a5
sonnenwind2008-png <sonnenwind2008@googlemail.com>
2026-03-10 17:55:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,4 +3,11 @@ self.addEventListener('install', (e) => {
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});self.addEventListener('install', (e) => {
+  console.log('Service Worker installiert');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Erforderlich für PWA-Erkennung
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
