e18fa51a92c46ba6002a7cdedc904aa20c03c9db
sonnenwind2008-png <sonnenwind2008@googlemail.com>
2026-03-16 17:59:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
-const CACHE_NAME = 'pdf-toolbox-v1';
+const CACHE_NAME = 'pdf-toolbox-v2'; // Version erhöht
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Installieren & Dateien in den Cache laden
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Aktivieren & alten Cache löschen
 self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
 
-// WICHTIG: Der Fetch-Handler (ohne diesen kein Install-Button!)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
